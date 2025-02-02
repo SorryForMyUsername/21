bedf733fdf7c17b6c10e8869962ecd3a8815e124
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-02-02 22:17:38 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,16 +10,21 @@ namespace _21_1
     {
         static void Main(string[] args)
         {
+            // Создание объекта класс с использованием
+            // конструктора без параметров и присваивания значений полям объекта.
             Calculate calc1 = new Calculate();
             calc1.first = 1.5;
             calc1.second = 0.5;
             Console.WriteLine($"{calc1}\n" +
                 $"Результат обработки значений полей: {calc1.HandleFields():F2}\n");
 
+            // Создание объекта класс с использованием явной инициализации.
             Calculate calc2 = new Calculate() { first = 3, second = 4};
             Console.WriteLine($"{calc2}\n" +
                 $"Результат обработки значений полей: {calc2.HandleFields():F2}\n");
 
+            // Создание объекта класс  с использованием конструктора с двумя параметрами,
+            // введенными с клавиатуры.
             Console.Write("Поле 1: ");
             double field1 = double.Parse(Console.ReadLine());
             Console.Write("Поле 2: ");
